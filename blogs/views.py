4aec27bec962499f5c8665d05818e8b9e85d96d9
niahizkia <niahizkia255@gmail.com>
2023-01-20 12:36:10 +0700

    basic way to get data from database to browser

--- blogs/views.py
+++ blogs/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
+from .models import Blog
 
 def index(request):
-    return HttpResponse('Hello you are in /blogs now...')
+    blogs = Blog.objects.all()
+    output = ', '.join([str(blog) for blog in blogs])
+    return HttpResponse(output)
