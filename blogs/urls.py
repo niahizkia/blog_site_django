4972ba8c48c1ef60c1d90c88dff6f8126328a87d
niahizkia <niahizkia255@gmail.com>
2023-01-23 21:25:38 +0700

    custom error page

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,7 +1,10 @@
 from . import views
 from django.urls import path
+from django.conf.urls import handler404, handler500
 
 urlpatterns = [
     path('', views.index),
     path('<int:id>', views.single),
 ]
+
+hander404 = 'views.handler404'
--- blogs/views.py
+++ blogs/views.py
@@ -1,12 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from .models import Blog
+
+
+def handler404(request):
+    return render(request, '404.html', status=404)
 
 def index(request):
     blogs = Blog.objects.all()
     return render(request, 'blogs/index.html', {'blogs':blogs})
 
-
 def single(request, id):
-    blog = Blog.objects.get(pk = id)
+    blog = get_object_or_404(Blog, pk=id)
     return render(request, 'blogs/single.html', {'blog':blog})
