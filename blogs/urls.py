b61f39aa0fd674cfd2e5601054610f55af9e8ea1
niahizkia <niahizkia255@gmail.com>
2023-01-23 20:59:05 +0700

    single view (comment feature)

--- blogs/urls.py
+++ blogs/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.index),
+    path('<int:id>', views.single),
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -4,5 +4,9 @@ from .models import Blog
 
 def index(request):
     blogs = Blog.objects.all()
-    output = ', '.join([str(blog) for blog in blogs])
     return render(request, 'blogs/index.html', {'blogs':blogs})
+
+
+def single(request, id):
+    blog = Blog.objects.get(pk = id)
+    return render(request, 'blogs/single.html', {'blog':blog})
