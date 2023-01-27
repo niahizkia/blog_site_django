de24974a1087a34a3b9f4ed493e9fdc7a1ea30be
niahizkia <niahizkia255@gmail.com>
2023-01-27 12:56:32 +0700

    Error Validator

--- blog_site/forms.py
+++ blog_site/forms.py
@@ -1,6 +1,16 @@
 from django import forms
+from django.core import validators
 
 class ContactForm(forms.Form):
     name = forms.CharField()
     email = forms.EmailField()
     subject = forms.CharField(widget=forms.Textarea)
+    honeypot = forms.CharField(widget=forms.HiddenInput, required=False, 
+                                validators=[validators.MaxLengthValidator(0)]) #Validator from django
+
+    # --------------Costum Validator--------------
+    # def clean_honeypot(self):
+    #     honeypot = self.cleaned_data['honeypot']
+    #     if len(honeypot):
+    #         raise forms.ValidationError("Shouldn't be filled!!!")
+    #     return honeypot
