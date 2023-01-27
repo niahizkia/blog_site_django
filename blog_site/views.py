b8fc8cd2eb929488cf72435dab275e3df6481e5c
niahizkia <niahizkia255@gmail.com>
2023-01-27 12:41:33 +0700

    sending an email

--- blog_site/views.py
+++ blog_site/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 from django.contrib import messages
+from django.core.mail import send_mail
 from . import forms
 
 def welcome(request):
@@ -14,6 +15,13 @@ def contact(request):
         form = forms.ContactForm(request.POST)
         if form.is_valid():
             # sending email
+            send_mail(
+                'dari kontak website',
+                request.POST['subject'],
+                request.POST['email'],
+                ['niahizkiaa@gmail.com'],
+                fail_silently=False,
+            )
             messages.success(request, 'email sent..')
             return HttpResponseRedirect(reverse('contact'))
 
