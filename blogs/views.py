142c5d514f3979328dd428e0a14eb3c393806948
niahizkia <niahizkia255@gmail.com>
2023-01-28 13:03:25 +0700

    modelform in django

--- blogs/views.py
+++ blogs/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.contrib import messages
 from .models import Blog
+from . import forms
 
 
 def handler404(request):
@@ -12,18 +15,18 @@ def index(request):
 
 def single(request, id):
     blog = get_object_or_404(Blog, pk=id)
-    return render(request, 'blogs/single.html', {'blog':blog})
+    form = forms.CommentForm()
+    return render(request, 'blogs/single.html', {'blog':blog, 'form':form})
 
 def comment(request, id):
     blog = get_object_or_404(Blog, pk=id)
 
     if request.method == 'POST':
-        newDesc = request.POST['desc']
-        
-        if len(newDesc) < 10:
-            return render(request, 'blogs/single.html', {
-                'blog':blog,
-                'errors':'komentar min 10 karakter'
-            })
-        blog.comment_set.create(desc=newDesc)
-        return render(request, 'blogs/single.html', {'blog':blog})
+        form = forms.CommentForm(request.POST)
+        if form.is_valid():
+            newDesc = request.POST['desc']
+            blog.comment_set.create(desc=newDesc)
+            
+            messages.success(request, 'Comment sent..')
+            return HttpResponseRedirect(reverse('blogs:index'))
+        # return render(request, 'blogs/single.html', {'blog':blog})
