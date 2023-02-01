2d294323ff9284ee4cbc34f652e2697a5467a9b5
niahizkia <niahizkia255@gmail.com>
2023-02-01 17:10:59 +0700

    edit and update modelform

--- blogs/urls.py
+++ blogs/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('<int:id>', views.single, name='single'),
     path('comment/<int:id>', views.comment, name='comment'),
+    path('comment-edit/<int:id>', views.comment_edit, name='comment-edit'),
 ]
 
 hander404 = 'views.handler404'
--- blogs/views.py
+++ blogs/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.contrib import messages
-from .models import Blog
+from .models import Blog, Comment
 from . import forms
 
 
@@ -21,6 +21,7 @@ def single(request, id):
 def comment(request, id):
     blog = get_object_or_404(Blog, pk=id)
     form = forms.CommentForm()
+
     if request.method == 'POST':
         form = forms.CommentForm(request.POST)
         if form.is_valid():
@@ -31,3 +32,16 @@ def comment(request, id):
             return HttpResponseRedirect(reverse('blogs:index'))
 
     return render(request, 'blogs/single.html', {'blog':blog, 'form':form})
+
+def comment_edit(request, id):
+    comment = get_object_or_404(Comment, pk=id)
+    form = forms.CommentForm(instance=comment)
+
+    if request.method == 'POST':
+        form = forms.CommentForm(instance=comment, data=request.POST)
+        if form.is_valid():
+            form.save()            
+            messages.success(request, 'Comment edited..')
+            return HttpResponseRedirect(reverse('blogs:index'))
+            
+    return render(request, 'blogs/comment_edit.html', {'comment':comment, 'form':form})
